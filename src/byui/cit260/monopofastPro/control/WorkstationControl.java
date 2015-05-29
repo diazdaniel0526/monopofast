752e2f54d39ce3d0d73f4bd7bbe198a105f8372e
Galicia <Galicia@Danny>
2015-05-29 11:17:27 -0600

    5/29 - started the first control

---
+++ src/byui/cit260/monopofastPro/control/WorkstationControl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.monopofastPro.control;
+
+/**
+ *
+ * @author Galicia
+ */
+public class WorkstationControl {
+    
+    public double calcTimeToCompleteOrder(double cooking, double, washing) {
+        
+        if (cooking <= 0) {
+		return -1;
+        }
+        
+	if (washing <= 0) {
+                return -1;
+        }        
+	time = cooking * 6 + washing
+	RETURN time
+
+    }
+    
+}
