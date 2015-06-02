3e668d1115cb4a2f0e8b19ae5ca6db22aa4a8cee
Galicia <Galicia@Danny>
2015-06-02 11:20:28 -0600

    06/02 -- created the Startprogramview class

---
+++ src/byui/cit260/monopofastPro/view/StartProgramView.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.monopofastPro.view;
+
+/**
+ *
+ * @author Galicia
+ */
+public class StartProgramView {
+    public StartProgramView () {
+}
+
+
+public void startProgram() {
+        // display the banner screen
+        this.displayBanner();
+        
+}
+
+    private void displayBanner() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
