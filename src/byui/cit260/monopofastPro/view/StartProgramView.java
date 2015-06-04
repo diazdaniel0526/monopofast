3f08819b2abf55d7a0ee7f8d4c65702bf2ee1d6c
masterDeveloper <ellisoverson@10.18.149.157>
2015-06-04 11:13:52 -0600

    Created get player name and welcome message  4 June 2015

--- src/byui/cit260/monopofastPro/view/StartProgramView.java
+++ src/byui/cit260/monopofastPro/view/StartProgramView.java
@@ -5,6 +5,10 @@
  */
 package byui.cit260.monopofastPro.view;
 
+import byui.cit260.monopofastPro.control.ProgramControl;
+import byui.cit260.monopofastPro.model.Player;
+import java.util.Scanner;
+
 /**
  *
  * @author Galicia
@@ -17,8 +21,29 @@ public class StartProgramView {
 public void startProgram() {
         // display the banner screen
         this.displayBanner();
+    
+        String playersName = this.getPlayersName();
+        
+        //create and save the player object
+        
+        Player player = ProgramControl.createPlayer(playersName);
+        
+        //display a welcome message
+        
+        this.displayWelcomeMessage(player);
+        
+        //display the main menu
+        
+        
         
 }
+    public void displayWelcomeMessage(Player player) {
+        System.out.println("\n\n==========================");
+        System.out.println("\tWelcome to the game" + player.getNameOfPlayer());
+        System.out.println("\tHave fun, beat your best score!");
+        System.out.println("=============================");
+    }
+
 
     private void displayBanner() {
      System.out.println("\n\n**************************************");
@@ -51,4 +76,37 @@ public void startProgram() {
      System.out.println("*****************************************");
      
     }
+
+    private String getPlayersName() {
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean valid = false; // indicates if the name has been retrieved
+        String playersName = null;
+        Scanner keyboard = new Scanner(System.in); // keboard input stream
+        
+        while(!valid) { //while a valid name has not been retrieved
+            
+            //prompt for the players name
+            System.out.println("Enter the player's name below:");
+            
+            //get the name form the keobard and trim off the blanks
+            playersName = keyboard.nextLine();
+            playersName = playersName.trim();
+            
+            //if the name is invalid (less than to character in length))
+            if (playersName.length() < 2) {
+                System.out.println("INvalid name - the name must not be blank");
+                continue; // and repeat again
+            }
+            break; // out of the (exit) the repetition
+        }
+        
+        return playersName; // return name
+       
+    }
+    
+    public static Player createPlayer(String playersName){
+        System.out.println("\n**** createPlayer function called ****");
+        return null;
+    }
+
 }
